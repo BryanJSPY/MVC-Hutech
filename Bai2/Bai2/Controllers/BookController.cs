284d5efe135f8ed25ee5fce224d091be0699199a
sinhvien <BryanJSPY>
2020-05-28 16:00:44 +0700

    Lab2

---
+++ Bai2/Bai2/Controllers/BookController.cs
@@ -0,0 +1,113 @@
+﻿using Bai2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bai2.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult ListBookModel()
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "aaa", "bbb", "/Content/Images/ccc.jpg"));
+            books.Add(new Book(2, "aaa1", "bbb1", "/Content/Images/ccc.jpg"));
+            books.Add(new Book(3, "aaa2", "bbb2", "/Content/Images/ccc.jpg"));
+            return View(books);
+        }
+
+        public ActionResult EditBook(int? id)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "aaa", "bbb", "/Content/Images/ccc.jpg"));
+            books.Add(new Book(2, "aaa1", "bbb1", "/Content/Images/ccc.jpg"));
+            books.Add(new Book(3, "aaa2", "bbb2", "/Content/Images/ccc.jpg"));
+
+            Book book = new Book();
+
+            foreach(Book b in books)
+            {
+                if(b.Id == id)
+                {
+                    book = b;
+                    break;
+                }
+            }
+
+            if(book == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(book);
+        }
+
+        [HttpPost, ActionName("Editbook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditBook(int? id, string Title, string Author, string ImageCover)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "aaa", "bbb", "/Content/Images/ccc.jpg"));
+            books.Add(new Book(2, "aaa1", "bbb1", "/Content/Images/ccc.jpg"));
+            books.Add(new Book(3, "aaa2", "bbb2", "/Content/Images/ccc.jpg"));
+
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+
+            foreach (Book b in books)
+            {
+                if (b.Id == id)
+                {
+                    b.Title = Title;
+                    b.Author = Author;
+                    b.ImageCover = ImageCover;
+                    break;
+                }
+            }
+
+
+            return View("ListBookModel", books);
+        }
+
+        public ActionResult CreateBook()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("CreateBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact([Bind(Include ="Id,Title,Author,ImageCover")]Book book)
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "aaa", "bbb", "/Content/Images/ccc.jpg"));
+            books.Add(new Book(2, "aaa1", "bbb1", "/Content/Images/ccc.jpg"));
+            books.Add(new Book(3, "aaa2", "bbb2", "/Content/Images/ccc.jpg"));
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    books.Add(book);
+                }
+            }
+            catch (RetryLimitExceededException)
+            {
+                ModelState.AddModelError("", "Error Save Data");
+            }
+
+
+            return View("ListBookModel", books);
+        }
+
+        public ActionResult Details(Book book)
+        {
+            return View(book);
+        }
+    }
+}
---
+++ Bai2/Bai2/Models/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bai2.Models
+{
+    public class Book
+    {
+        public Book()
+        {
+
+        }
+
+        public Book(int id, string title, string author, string imageCover)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            ImageCover = imageCover;
+        }
+        [Display(Name = "Ma Sach")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Tieu de khong duoc trong")]
+        [StringLength(250,ErrorMessage ="Tieu de khong vuot qua 250 ki tu")]
+        [Display(Name ="Tieu De")]
+        public string Title { get; set; }
+        [Display(Name = "Tac Gia")]
+        public string Author { get; set; }
+        [Display(Name = "Duong Dan Anh")]
+        public string ImageCover { get; set; }
+    }
+
+}
